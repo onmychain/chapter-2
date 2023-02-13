45305fd23ac89cf4a6f0b009f024d63806501833
Cyrille Derche <c.derche@me.com>
2023-02-13 19:29:25 +0100

    deposit

--- blockchain/contracts/Staking.sol
+++ blockchain/contracts/Staking.sol
@@ -9,8 +9,15 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Staking {
 
     IERC20 public immutable token;
+    uint public immutable rewardsPerHour = 100000000000000; // 0.01%
 
     uint public stakeBalance = 0;
+
+    event Deposit(address sender, uint amount);
+
+    mapping(address => uint) public balanceOf;
+    mapping(address => uint) public lastUpdated;
+    mapping(address => uint) public claimed;
 
     constructor(IERC20 token_) {
         token = token_;
@@ -22,6 +29,18 @@ contract Staking {
 
     function _rewardBalance() internal view returns (uint) {
         return token.balanceOf(address(this)) - stakeBalance;
+    }
+
+    function deposit(uint amount_) external {
+        _deposit(amount_);
+    }
+
+    function _deposit(uint amount_) internal {
+        token.transferFrom(msg.sender, address(this), amount_);
+        balanceOf[msg.sender] += amount_;
+        lastUpdated[msg.sender] = block.timestamp;
+        stakeBalance += amount_;
+        emit Deposit(msg.sender, amount_);
     }
 
 }
--- blockchain/test/Staking.ts
+++ blockchain/test/Staking.ts
@@ -1,4 +1,5 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers"
+import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { expect } from "chai"
 import { Contract } from "ethers"
 import { ethers } from "hardhat"
@@ -39,5 +40,71 @@ describe("Staking", function () {
         it("Should have 0 stake balance", async function () {
             expect(await staking.stakeBalance()).to.eq(0)
         })
+        it("Should have 0.1% rewards per second", async function () {
+            expect(await staking.rewardsPerSecond()).to.eq(ethers.utils.parseEther("0.0001"))
+        })
     })
+
+    describe("Deposit", function () {
+
+        let token: Contract, staking: Contract
+        let signer: SignerWithAddress
+        let amount = ethers.utils.parseEther("100000")
+
+        beforeEach(async function () {
+            [token, staking] = await loadFixture(deployFixture)
+            const signers = await ethers.getSigners()
+            signer = signers[0]
+            await token.approve(staking.address, amount)
+        })
+
+        it("Should transfer amount", async function () {
+            await expect(staking.deposit(amount)).to.changeTokenBalances(token, 
+                [signer, staking],
+                [amount.mul(-1), amount]
+            )
+        })
+        it("Should have a balanceOf(address) equal to amount transfered", async function () {
+            await staking.deposit(amount)
+            expect(await staking.balanceOf(signer.address)).to.eq(amount)
+        })
+        it("Should have a lastUpdated(address) equal to the latest block timestamp", async function () {
+            await staking.deposit(amount)
+            const latest = await time.latest()
+            expect(await staking.lastUpdated(signer.address)).to.eq(latest)
+        })
+        it("Should have 0 claimed rewards", async function () {
+            await staking.deposit(amount)
+            expect(await staking.claimed(signer.address)).to.eq(0)
+        })
+        it("Should increase the stake balance by amount", async function () {
+            await staking.deposit(amount)
+            expect(await staking.stakeBalance()).to.eq(amount)
+        })
+        it("Should not change the reward balance", async function () {
+            await staking.deposit(amount)
+            expect(await staking.rewardBalance()).to.eq(initialRewards)
+        })
+
+        describe("Validations", function () {
+            it("Should revert if staking address not approved", async function () {
+                amount = ethers.utils.parseEther("1000000")
+                await expect(staking.deposit(amount)).to.be.reverted
+            })
+            it("Should revert if address has insufficient balance", async function () {
+                await token.approve(staking.address, totalSupply)
+                await expect(staking.deposit(totalSupply)).to.be.reverted
+            })
+        })
+
+        describe("Events", function () {
+            it("Should emit Deposit event", async function () {
+                await expect(staking.deposit(amount)).to.emit(staking, "Deposit").withArgs(
+                    signer.address, amount
+                )
+            })
+        })
+
+    })
+
 })
